6b99aaf5372f22793475b727530817aeb2aa180d
Souradeep <souradeepkrbasu@gmail.com>
2020-07-29 18:46:14 +0530

    Update

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 //HTML Elements
 const question = document.querySelector("#question");
+const questionNumber = document.querySelector("#questionNumber");
 const solvedQuestion = document.querySelector("#solved");
 const unsolvedQuestion = document.querySelector("#unsolved");
 const solvedLabel = document.querySelector("#solvedCounter");
 const unsolvedLabel = document.querySelector("#unsolvedCounter");
 const stats = document.querySelector("#stats");
 const buttons = document.querySelector("#buttons");
+
+questionNumber.innerHTML=1;
 
 //Counters
 let solvedCounter = 0;
@@ -32,12 +35,14 @@ displayQuestion();
 
 //Event listeners
 solvedQuestion.addEventListener("click", () => {
+  questionNumber.innerHTML=questionCounter+1;
   displayQuestion();
   ++solvedCounter;
   solvedLabel.innerHTML = solvedCounter;
 });
 
 unsolvedQuestion.addEventListener("click", () => {
+  questionNumber.innerHTML=questionCounter+1;
   unsolvedQuestions[unsolvedCounter] = questionCounter;
   displayQuestion();
   ++unsolvedCounter;
