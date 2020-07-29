766b0fb1f40499bba319153230dc588a81500d32
Souradeep <souradeepkrbasu@gmail.com>
2020-07-29 21:14:09 +0530

    Update

--- main.js
+++ main.js
@@ -7,8 +7,10 @@ const solvedLabel = document.querySelector("#solvedCounter");
 const unsolvedLabel = document.querySelector("#unsolvedCounter");
 const stats = document.querySelector("#stats");
 const buttons = document.querySelector("#buttons");
+const showSummary = document.querySelector("#summary");
+const table = document.querySelector("#table");
 
-questionNumber.innerHTML=1;
+questionNumber.innerHTML = 1;
 
 //Counters
 let solvedCounter = 0;
@@ -18,9 +20,28 @@ let unsolvedQuestions = [];
 
 //summary
 const summary = () => {
-  question.innerHTML = "Summary";
-  stats.style.display = "none";
+  question.innerHTML = "Your summary";
   buttons.style.display = "none";
+  stats.style.display = "none";
+  table.innerHTML = `
+  <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Question</th>
+    </tr>
+  </thead>
+  <tbody>
+  ${unsolvedQuestions.map(
+    (question) =>
+      `    <tr>
+                <th scope="row">${question+1}</th>
+                <td>${easyQuestions[question]}</td>
+           </tr>`
+  )}
+
+  </tbody>
+</table>`;
 };
 
 //Display question
@@ -35,17 +56,20 @@ displayQuestion();
 
 //Event listeners
 solvedQuestion.addEventListener("click", () => {
-  questionNumber.innerHTML=questionCounter+1;
+  questionNumber.innerHTML = questionCounter + 1;
   displayQuestion();
   ++solvedCounter;
   solvedLabel.innerHTML = solvedCounter;
 });
 
 unsolvedQuestion.addEventListener("click", () => {
-  questionNumber.innerHTML=questionCounter+1;
-  unsolvedQuestions[unsolvedCounter] = questionCounter;
+  questionNumber.innerHTML = questionCounter + 1;
+  unsolvedQuestions[unsolvedCounter] = questionCounter - 1;
   displayQuestion();
   ++unsolvedCounter;
-  console.log(unsolvedQuestions);
   unsolvedLabel.innerHTML = unsolvedCounter;
+});
+
+showSummary.addEventListener("click", () => {
+  summary();
 });
